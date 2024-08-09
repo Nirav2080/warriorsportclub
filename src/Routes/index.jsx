d9483c2ca468a2be7c590dea25a718748ea62b72
nirav <niravmangukiya88@gmail.com>
2024-08-09 10:07:25 +0530

    Add header and contact page

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,17 +2,20 @@ import Home from "../Component/Home";
 // import Service from "../Components/Home/Services";
 import { Route, Routes } from "react-router-dom";
 // import Footer from "../Component/Footer";
-// import Navbar from "../Component/Navbar";
+import Footer from "../Component/Footer";
+import Navbar from "../Component/Navbar";
+import ContactUs from "../Pages/Contact Us";
 
 const AppRoutes = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/service" element={<Service />} /> */}
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
