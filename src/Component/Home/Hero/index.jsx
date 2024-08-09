0d747bbe2de1fcbd0c66802c8bd55093c606c3b3
Shashwat Chovatiya <132459393+shashwatchovatiya@users.noreply.github.com>
2024-08-09 16:15:07 +0530

    Hero-Section-Slider

--- src/Component/Home/Hero/index.jsx
+++ src/Component/Home/Hero/index.jsx
@@ -1,5 +1,103 @@
-const Hero = () => {
-  return <div>fnueitgnaf;ieshuolzrins.eklgihre;insgu9jol</div>;
-};
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 
-export default Hero;
+import slider1 from "../../../assets/Slider-1.jpg";
+import slider2 from "../../../assets/Slider-2.jpg";
+
+import slider3 from "../../../assets/Slider-3.jpg";
+import slider4 from "../../../assets/Slider-4.jpg";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const SwiperData = [
+  {
+    id: 1,
+    img: slider1,
+    alt: "Slider 1",
+    heading: "Our main Facilities",
+    text: "Discover our exceptional facilities and services at Warrior Sports Club.",
+  },
+  {
+    id: 2,
+    img: slider2,
+    alt: "Slider 2",
+    heading: "Our main Facilities",
+    text: "Discover our exceptional facilities and services at Warrior Sports Club.",
+  },
+  {
+    id: 3,
+    img: slider3,
+    alt: "Slider 1",
+    heading: "Premier Basketball Courts",
+    text: "Shoot some hoops on our professionally designed courts, perfect for both casual games and competitive tournaments.",
+  },
+  {
+    id: 4,
+    img: slider4,
+    alt: "Slider 1",
+    heading: "Premier Basketball Courts",
+    text: "Shoot some hoops on our professionally designed courts, perfect for both casual games and competitive tournaments.",
+  },
+];
+
+import bgImage from "../../../assets/HeroBackground.webp";
+function index() {
+  return (
+    <div className="">
+      <div className="flex justify-center py-10 overflow-hidden">
+        <Swiper
+          className="h-auto md:h-full "
+          navigation={{
+            nextEl: ".swiper-button-next-custom",
+            prevEl: ".swiper-button-prev-custom",
+          }}
+          loop={true}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          modules={[Navigation, Pagination, A11y, Autoplay]}
+          autoplay={{ delay: 300000 }}
+        >
+          {SwiperData?.map((data, index) => (
+            <SwiperSlide
+              className="w-full flex justify-center relative"
+              key={index}
+            >
+              <img
+                src={data.img}
+                alt={data.alt}
+                loading="lazy"
+                className="h-screen w-full object-cover z-10"
+              />
+
+              {/* Dark Overlay */}
+              <div className="absolute inset-0 z-20 bg-black bg-opacity-60 flex items-center justify-center">
+                {/* Text Overlay */}
+                <div className="text-center p-10 font-bold text-white">
+                  <h2 className="text-4xl font-bold mb-4">{data.heading}</h2>
+                  <p className="text-lg">{data.text}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* Custom Arrow */}
+        <div className="swiper-button-prev-custom absolute left-0 top-[70%] transform  bg-white text-black p-2 md:text-3xl text-xl rounded-full cursor-pointer z-10">
+          <MdKeyboardArrowLeft />
+        </div>
+        <div className="swiper-button-next-custom absolute right-0 top-[70%] transform  bg-white text-black p-2 md:text-3xl text-xl rounded-full cursor-pointer z-10">
+          <MdKeyboardArrowRight />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default index;
--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="relative w-full bg-gray-400 py-6">
+    <div className="relative w-full bg-white py-6">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
         <div className="inline-flex items-center space-x-2">
           <span>
